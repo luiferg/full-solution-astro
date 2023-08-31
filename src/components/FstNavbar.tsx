387382390e649b2e38c592ea436c040edea016d3
Luis Gomez <luisfgomezortiz@gmail.com>
2023-08-31 22:37:04 +0200

    Implemented navbar with react, created some astro layouts

---
+++ src/components/FstNavbar.tsx
@@ -0,0 +1,90 @@
+'use client'
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
+  Link,
+} from '@nextui-org/react'
+import { useState } from 'react'
+import { motion } from 'framer-motion'
+import { Image } from 'astro:assets'
+
+const FstNavbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const links = [
+    { href: '/portfolio', label: `Portfolio` }, // portfolio link
+    { href: '/about', label: `About` }, // about link
+    { href: '/blog', label: `Blog` }, // blog link
+  ]
+
+  return (
+    <Navbar
+      isMenuOpen={isMenuOpen}
+      onMenuOpenChange={setIsMenuOpen}
+      maxWidth='xl'
+      shouldHideOnScroll={true}
+    >
+      <NavbarContent className='hidden sm:flex gap-4 lg:gap-8'>
+        {links.map((link) => (
+          <NavbarItem key={link.href}>
+            <Link
+              href={link.href}
+              className='text-fst-black text-base'
+            >{link.label}</Link>
+          </NavbarItem>
+        ))}
+      </NavbarContent>
+      <NavbarContent justify='center'>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
+          className='sm:hidden'
+        />
+        <NavbarBrand>
+          <Link href='/' onClick={() => setIsMenuOpen(false)}>
+            <img
+              src='/fst-logo.svg'
+              width={200}
+              height={100}
+              alt='FullSolution Telecom logo'
+              className='w-48 md:w-56 h-auto'
+            />
+          </Link>
+        </NavbarBrand>
+      </NavbarContent>
+      <NavbarContent justify='end'>
+        <NavbarItem>
+        </NavbarItem>
+        <NavbarItem className='max-sm:hidden'>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarMenu>
+        {links.map((link, index) => (
+          <NavbarMenuItem key={index}>
+            <Link
+              className='w-full text-fst-black text-base'
+              href={link.href}
+              onPress={() => setIsMenuOpen(false)}
+            >
+              {link.label}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+        <NavbarMenuItem>
+          <Link
+            href='/#contact'
+            className='text-fst-orange-400 font-semibold text-base'
+            onPress={() => setIsMenuOpen(false)}
+          >
+          </Link>
+        </NavbarMenuItem>
+      </NavbarMenu>
+    </Navbar>
+  )
+}
+
+export default FstNavbar
---
+++ tailwind.config.cjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+const { nextui } = require("@nextui-org/react");
+
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+	"./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+],
+	theme: {
+		extend: {
+			colors: {
+        'fst-orange-300': '#f76c1b',
+        'fst-orange-400': '#eb661b',
+        'fst-orange-500': '#d15b17',
+        'fst-orange-600': '#ab4b13',
+        'fst-grey': '#959799',
+        'fst-black': '#141414',
+      },
+      fontFamily: {
+        secondary: ['Nevo', 'sans-serif'],
+      },
+		},
+	},
+	darkMode: "class",
+  plugins: [nextui()]
+}
